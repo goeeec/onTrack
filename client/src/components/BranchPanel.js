5049e085afe5e7eee20107c218ec35e865ea5f38
Joey Ching <joeyching1@gmail.com>
2018-12-03 00:39:18 -0500

    removed unnecessary import

--- client/src/components/BranchPanel.js
+++ client/src/components/BranchPanel.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Row, Col } from "react-materialize";
 import { Grid } from "@material-ui/core";
 import TodoList from "./TodoList";
 class BranchPanel extends Component {
