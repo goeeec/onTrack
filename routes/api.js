a39cf814d3648a7fc34b61c0d0ba2ae2c0a09b44
Joey Ching <joeyching1@gmail.com>
2018-12-12 16:18:51 -0500

    new repo is now saved to DB

--- routes/api.js
+++ routes/api.js
@@ -18,37 +18,37 @@ router.get("/project/:id", async (req, res) => {
 router.post("/project", async (req, res) => {
   const existingProject = await Project.findOne({ where: { projectId: req.body.projectId + '' } });
   if (existingProject) {
-    res.send(400, 'Duplicated project');
-  }
-  //console.log('Creating new Project: ', req.body);
-  res.sendStatus(201);
-  try {
-    let project = await Project.create({
-      projectId: req.body.projectId + '',   // change to string type
-      name: req.body.name,
-      description: req.body.description,
-      cloneUrl: req.body.cloneUrl,
-      owner: req.body.owner,
-    });
-    console.log("Now mapping branches to project id: ", project.id);
-    console.log("Branch number: ", req.body.branches.length);
-    req.body.branches.map(async branch => {
-      try {
-        console.log("Received branch object: ", branch);
-        existingBranch = await Branch.findOne({ where: { nodeId: branch.nodeId } });
-        if (!existingBranch) {
-          await Branch.create({
-            name: branch.name,
-            sha: branch.sha,
-            location: branch.location,
-            nodeId: branch.nodeId,
-            projectId: project.id
-          });
-        }
-      } catch(err) { console.log(err) }
-    })
-  } catch(err) {
-    console.log(err);
+    res.status(400).send('Duplicated project');
+  } else {
+    try {
+      let project = await Project.create({
+        projectId: req.body.projectId + '',   // change to string type
+        name: req.body.name,
+        description: req.body.description,
+        cloneUrl: req.body.cloneUrl,
+        owner: req.body.owner,
+      });
+      console.log("Now mapping branches to project id: ", project.id);
+      console.log("Branch number: ", req.body.branches.length);
+      req.body.branches.map(async branch => {
+        try {
+          console.log("Received branch object: ", branch);
+          existingBranch = await Branch.findOne({ where: { nodeId: branch.nodeId } });
+          if (!existingBranch) {
+            await Branch.create({
+              name: branch.name,
+              sha: branch.sha,
+              location: branch.location,
+              nodeId: branch.nodeId,
+              projectId: project.id
+            });
+          }
+        } catch(err) { console.log(err) }
+      });
+      res.sendStatus(201);
+    } catch(err) {
+      console.log(err);
+    }
   }
 })
 
